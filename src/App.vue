a055f27691919cdf0ecc223f6e8c68be127e5e80
friedmanrivka <rivkaf291@gmail.com>
2024-10-12 23:42:02 +0300

    Initialized project of Vue, created Zero Trust component, and added company data

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div v-for="company in companies" :key="company.companyName" class="company-widget">
+      <h1>{{ company.companyName }}</h1>
+      
+      <!-- הצגת ציון האמון האפסי -->
+      <v-progress-circular :value="company.ZeroTrustScore" color="blue" size="100" width="15">
+        {{ company.ZeroTrustScore }}
+      </v-progress-circular>
+
+      <!-- הצגת קטגוריית הסיכון -->
+      <h2>Risk Category: {{ company.riskCategory }}</h2>
+
+      <!-- הצגת מדדים נוספים -->
+      <v-progress-linear :value="company.metrics.ShannonEntropyScore * 10" color="green">
+        Shannon Entropy Score: {{ company.metrics.ShannonEntropyScore }}
+      </v-progress-linear>
+      <p>Firewall Detected: {{ company.metrics.firewallDetected ? 'Yes' : 'No' }}</p>
+      <p>Open Ports Detected: {{ company.metrics.openPortsDetected }}</p>
+      <p>TLS Version: {{ company.metrics.tlsVersion }}</p>
+      <p>Security Headers: {{ company.metrics.securityHeadersImplemented.join(', ') }}</p>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.company-widget {
+  margin-bottom: 40px;
 }
 </style>
