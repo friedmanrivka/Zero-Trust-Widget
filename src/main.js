9b91fba9523d9c3106e12e9a6be3742e1f27dbec
friedmanrivka <rivkaf291@gmail.com>
2024-10-28 18:03:18 +0200

    deleting draft lines

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import vuetify from './plugins/vuetify'; // ייבוא הגדרות ה-Vuetify
+import vuetify from './plugins/vuetify'; 
 
-import 'vuetify/styles'; // ייבוא הסגנונות של Vuetify
-import '@mdi/font/css/materialdesignicons.css'; // אייקונים של Material Design Icons
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css'; 
 
 createApp(App)
   .use(vuetify)
